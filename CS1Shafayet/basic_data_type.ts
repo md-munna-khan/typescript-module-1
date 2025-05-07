1eb39be8f429197dc0dd1f83d499dcef039b27fe
md.munna <mk8761174@gmail.com>
2025-05-07 19:31:17 +0600

    cs1 practice continue

---
+++ CS1Shafayet/basic_data_type.ts
@@ -0,0 +1,26 @@
+{
+    //
+const name:string="munna"
+const number: number[]=[3,5,8]
+
+
+// type allies
+type Obj={
+    name: string;
+    age: number;
+    education: {
+        institution: "college" | "varsity";
+        level?: string;
+    };}
+
+const obj:Obj  = {
+    name: 'Mansur',
+    age: 21,
+    education: {
+        institution: 'varsity',
+        // level: "Undergrad"
+    }
+}
+
+    //
+}
---
+++ CS1Shafayet/function.ts
@@ -0,0 +1,16 @@
+{
+    //
+    function roll(param:number):number{
+        const multi= param *2
+        return multi
+        }
+console.log(roll(4))
+
+
+const num=(param:number):number=>{
+    const add =param + 10
+    return add
+}
+console.log(num(50))
+    //
+}
